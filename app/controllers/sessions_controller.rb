2856deb0fdb98cf372f7dbf21db23d2f68ab1aef
Karen V <karenvo@usc.edu>
2019-12-12 01:59:28 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if params[:name].valid?
+    if params[:name].empty?
     #   redirect_to 'login'
     # else
       session[:name] = params[:name] #creates username, saves it
